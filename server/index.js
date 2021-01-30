60e67c194880ccd432924c7b0e21f2a28d22c04f
JLopezz3 <lopezjerel@gmail.com>
2021-01-30 12:54:42 -0600

    create db instance on heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const db = require("./db/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
