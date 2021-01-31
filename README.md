e688f5c1cfed0bdc70594ec688f6e3c0712963d7
JLopezz3 <lopezjerel@gmail.com>
2021-01-31 09:01:44 -0600

    update files

--- README.md
+++ README.md
@@ -15,3 +15,12 @@
 2. `cd server`
 3. `npm i`
 4. `nodemon index.js OR node index.js`
+
+### Starting Database on MySQL Workbench
+
+1. `OPEN MySQL Workbench`
+2. `Click the + icon to Setup New Connection`
+3. `connection credentials can be found at server/db/db.js file`
+4. `enter Host, Username, and Password and click Test Connection.`
+5. `after successful connection, open up the connection`
+6. `Go to SCHEMAS tab, and click the dropdown arrow on heroku_0685896544e70c1 to view the schema. User can view all the Tables in the Database by RightClicking any table in "Tables" tab -> Click on "SELECT ROWS". `
--- client/src/pages/TicketSearch2/TicketSearch2.js
+++ client/src/pages/TicketSearch2/TicketSearch2.js
@@ -5,101 +5,195 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-function TicketSearch2() {
+export default function TicketSearch2({ allflight }) {
   return (
     <div>
       <VerticalTimeline layout={"1-column-left"}>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Feb 1, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
+          <h3 className="vertical-timeline-element-title">
+            Departing: HOU at 17:50
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: MIA at 19:00
+          </h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Jan 31, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
         >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
+          <h3 className="vertical-timeline-element-title">
+            Departing: MIA at 17:50
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: HOU at 19:00
+          </h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Feb 1, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
+          <h3 className="vertical-timeline-element-title">
+            Departing: HOU at 17:50
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: MIA at 19:00
+          </h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="April 2013"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Jan 31, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
         >
           <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
+            Departing: LAX at 9:50
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: JFK at 11:00
+          </h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Jan 31, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
         >
           <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
+            Departing: JFK at 9:50
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: LAX at 11:00
+          </h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-        />
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Feb 1, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Departing: LAX at 9:50
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: JFK at 11:00
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Jan 31, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Departing: ORD at 14:30
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: OKC at 16:00
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+          contentArrowStyle={{
+            borderRight: "7px solid  rgb(33, 150, 243)",
+          }}
+          date="Feb 1, 2021"
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff",
+          }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Departing: OKC at 14:30
+          </h3>
+          <h3 className="vertical-timeline-element-subtitle">
+            {" "}
+            Arriving: ORD at 16:00
+          </h3>
+        </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   );
 }
-
-export default TicketSearch2;
