330e5d3e111c2ea0919dc4df0e32e0193cb8a4d1
trinhamcity1 <trinhamcity1@gmail.com>
2021-01-30 23:42:17 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
